b114f5bf7bc9be2020f1f19b1d60bba52c5ebb6b
ilya <ilyamahrooghimath@gmail.com>
2022-01-14 17:07:44 +0000

    map of polygon is complete

--- include/myheader.h
+++ include/myheader.h
@@ -13,17 +13,52 @@ struct region
     Uint32 color;
 };
 
+void random_color_array(Uint32 color1, Uint32 color2, Uint32 color3, Uint32 color4, Uint32 colorarray[10][10])
+{
+    time_t t;
+    srand((unsigned)time(&t));
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            int tmp;
+            tmp = rand();
+            tmp %= 4;
+
+            switch (tmp)
+            {
+            case 0:
+                colorarray[i][j] = color1;
+                break;
+            case 1:
+                colorarray[i][j] = color2;
+                ;
+                break;
+            case 2:
+                colorarray[i][j] = color3;
+                break;
+            case 3:
+                colorarray[i][j] = color4;
+                break;
+            }
+            // colorarray[i][j] = random_color(color1, color2, color3, color4);
+        }
+    }
+}
+
 struct point rotate(struct point center, struct point torotate, double angle)
 {
     struct point ans;
-    double radius = (torotate.x - center.x) * (torotate.x - center.x) + (torotate.y - center.y) * (torotate.y - center.y);
-    radius = pow(radius , 0.5);
-    ans.x = radius * cos(angle) + torotate.x;
-    ans.y = radius * sin(angle) + torotate.y;
+    double tx = torotate.x - center.x;
+    double ty = torotate.y - center.y;
+    ans.x = tx * cos(angle) - ty * sin(angle);
+    ans.y = tx * sin(angle) + ty * cos(angle);
+    ans.x += center.x;
+    ans.y += center.y;
     return ans;
 }
 
-struct region *drawpolygonregion(SDL_Renderer *renderer, struct point center, int radius, Uint32 color)
+struct region *drawpolygonregion(SDL_Renderer *renderer, struct point center, double radius, Uint32 color)
 {
     int x = center.x;
     int y = center.y;
@@ -36,10 +71,51 @@ struct region *drawpolygonregion(SDL_Renderer *renderer, struct point center, in
     {
         xarray[i] = firstpoint.x;
         yarray[i] = firstpoint.y;
-        
-        firstpoint = rotate(center , firstpoint , 1.04);
+
+        firstpoint = rotate(center, firstpoint, 1.0471975); // 1.0471975 is pi /2;
+    }
+    for (int i = 0; i < 6; i++)
+    {
+        double width = 4;
+        thickLineRGBA(renderer, xarray[i], yarray[i], xarray[(i + 1) % 6], yarray[(i + 1) % 6], width, 0, 0, 0, 0xff);
     }
 
-    filledPolygonColor(renderer  ,xarray, yarray ,6 , color);
+    filledPolygonColor(renderer, xarray, yarray, 6, color);
+    struct region *poly = (struct region *)malloc(sizeof(struct region));
+    poly->x_center = center.x;
+    poly->y_center = center.y;
+    poly->color = color;
+    poly->numofsoldiers = 0;
+    return poly;
+}
 
+struct region *polygonwindow(SDL_Renderer *renderer, struct point firstcenter, const int screen_height, const int screen_width, Uint32 color[10][10])
+{
+    double radius = screen_width / 7.5;
+    double x_diff = 3 * radius;
+    double y_diff = radius * pow(3, 0.5);
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            struct point center;
+            center.x = firstcenter.x + x_diff * (i - 1);
+            center.y = firstcenter.y + y_diff * (j - 1);
+            drawpolygonregion(renderer, center, radius, color[i][j]);
+        
+        }
+    }
+    firstcenter.x += 3 * radius / 2;
+    firstcenter.y += radius * (pow(3, 0.5) / 2);
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            struct point center;
+            center.x = firstcenter.x + x_diff * i;
+            center.y = firstcenter.y + y_diff * (j - 1);
+            drawpolygonregion(renderer, center, radius, color[i + 3][j]);
+            
+        }
+    }
 }
--- src/main.c
+++ src/main.c
@@ -3,30 +3,21 @@
 
 #include <stdio.h>
 #include <stdbool.h>
-
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
 
 #include "myheader.h"
 
-
 int main()
 {
-  /* Additional Header
-
-  print();
-  struct Node mynode;
-  mynode.id = 5;
-  mynode.number = 0;
-  printf("node id is %d and its number is %lf\n", mynode.id, mynode.number);
-
-  */
-
   if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) < 0)
   {
     printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
     return 0;
   }
-  const int SCREEN_WIDTH = 1000;
-  const int SCREEN_HEIGHT = 1000;
+  const double SCREEN_WIDTH = 1000;
+  const double SCREEN_HEIGHT = 1000;
 
   ///////////// main
   SDL_Window *sdlWindow = SDL_CreateWindow("Test_Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
@@ -38,26 +29,23 @@ int main()
   SDL_Renderer *sdlRenderer = SDL_CreateRenderer(sdlWindow, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
   SDL_bool shallexit = SDL_FALSE;
   SDL_Event sdlevent;
+  Uint32 color[10][10];
+  random_color_array(0xff00ff00, 0xffff0000, 0xff0000ff, 0x50000000, color);
+  time_t t;
+  srand(time(&t));
+  struct point center;
+  center.x = 0;
+  center.y = 0;
   while (shallexit == SDL_FALSE)
   {
-	// reset the color
+    // reset the color
     SDL_SetRenderDrawColor(sdlRenderer, 0xff, 0xff, 0xff, 0xff);
     SDL_RenderClear(sdlRenderer);
 
+    // intialize the screen
+    polygonwindow(sdlRenderer, center, SCREEN_HEIGHT, SCREEN_WIDTH, color);
 
-	//intialize the screen
-	struct point center;
-	center.x = SCREEN_HEIGHT/2;
-	center.y = SCREEN_HEIGHT/2;
-	drawpolygonregion(sdlRenderer, center , 100 , 0xffffffff);
-
-
-
-
-
-
-
-	// render presentation and SDL_Quit
+    // render presentation and SDL_Quit
     SDL_RenderPresent(sdlRenderer);
     SDL_PollEvent(&sdlevent);
     if (sdlevent.type == SDL_QUIT)
@@ -66,9 +54,9 @@ int main()
     }
   }
 
-  SDL_Delay(1000);
+  SDL_Delay(1000 / FPS);
   SDL_DestroyWindow(sdlWindow);
-  printf("Hello World\n");
+  printf("Hope see you soon for the next game:)\n");
   SDL_Quit();
   return 0;
 }
